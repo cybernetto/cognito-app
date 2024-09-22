051ac213519fc74dc9872951f8bc78c62c2f332a
Raimundo Pessoa <net.almada@gmail.com>
2024-09-22 01:13:54 -0300

    clean code to authController

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -2,20 +2,106 @@ import { Context } from 'koa';
 import { CognitoUserPool, CognitoUser, AuthenticationDetails, CognitoUserAttribute } from 'amazon-cognito-identity-js';
 import { RequestBody } from '../interfaces/requestBody';
 import { User } from '../models/user'; 
-//import { getRepository } from 'typeorm';
-import { AppDataSource } from '../orm/ormconfig'
+import { AppDataSource } from '../orm/ormconfig';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const poolData = {
-  UserPoolId: "us-east-2_nITAHCIv0",
-  ClientId: "7kolr3304oibqonak39c5hd1mj"
+  UserPoolId: process.env.COGNITO_USER_POOL_ID || "",
+  ClientId: process.env.COGNITO_CLIENT_ID || "",
 };
+
+if (poolData.UserPoolId === "" || poolData.ClientId === "") {
+    throw new Error("COGNITO_USER_POOL_ID and COGNITO_CLIENT_ID must be defined in the environment variables.");
+}
 
 const userPool = new CognitoUserPool(poolData);
 
-export const signInOrRegister = async (ctx: Context) => {
-  const { password, confirm_password, confirm_code, email } = ctx.request.body as RequestBody;
+const confirmUser = async (cognitoUser: CognitoUser, confirm_code: string, ctx: Context) => {
+  return new Promise<void>((resolve) => {
+    cognitoUser.confirmRegistration(confirm_code, true, (err) => {
+      if (err) {
+        ctx.status = 500;
+        ctx.body = { message: 'Error confirming email.', error: err };
+      } else {
+        ctx.status = 200;
+        ctx.body = { message: 'Email confirmed successfully.' };
+      }
+      resolve();
+    });
+  });
+};
 
-  if (!password && !confirm_code && !confirm_password && !email) {
+const loginUser = async (cognitoUser: CognitoUser, password: string, ctx: Context) => {
+  return new Promise<void>((resolve) => {
+    cognitoUser.authenticateUser(new AuthenticationDetails({ Username: cognitoUser.getUsername(), Password: password }), {
+      onSuccess: (result) => {
+        ctx.status = 200;
+        ctx.body = { message: 'User authenticated successfully.', token: result.getAccessToken().getJwtToken() };
+        resolve();
+      },
+      onFailure: async (err) => {
+        if (err.code === 'NotAuthorizedException') {
+          await handleSignUp(cognitoUser, password, ctx);
+        } else if (err.code === "UserNotConfirmedException") {
+          ctx.status = 400;
+          ctx.body = { message: 'Please confirm the email' };
+          resolve();
+        } else {
+          ctx.status = 500;
+          ctx.body = { message: 'Authentication failed.', error: err };
+          resolve();
+        }
+      }
+    });
+  });
+};
+
+const handleSignUp = async (cognitoUser: CognitoUser, password: string, ctx: Context) => {
+  const { email } = ctx.request.body as RequestBody;
+  const attributeList = [new CognitoUserAttribute({ Name: 'email', Value: email })];
+
+  return new Promise<void>((resolve) => {
+    userPool.signUp(email, password, attributeList, [], async (signUpErr, signUpResult) => {
+      if (signUpErr) {
+        ctx.status = 500;
+        ctx.body = { message: 'Error signing up.', error: signUpErr };
+      } else if (signUpResult) {
+        await saveUserToDatabase(signUpResult.user, email, ctx);
+      } else {
+        ctx.status = 500;
+        ctx.body = { message: 'Sign-up failed: No result returned.' };
+      }
+      resolve();
+    });
+  });
+};
+
+const saveUserToDatabase = async (cognitoUser: CognitoUser, email: string, ctx: Context) => {
+  const newUser = new User();
+  newUser.cognitoId = cognitoUser.getUsername();
+  newUser.email = email;
+  newUser.role = 'user';
+  newUser.isOnboarded = false;
+  newUser.createdAt = new Date();
+  newUser.updatedAt = new Date();
+
+  try {
+    const userRepository = AppDataSource.getRepository(User);
+    await userRepository.save(newUser);
+    ctx.status = 201;
+    ctx.body = { message: 'User registered successfully, please confirm the email.' };
+  } catch (dbError) {
+    ctx.status = 500;
+    ctx.body = { message: 'Error saving user to database.', error: dbError };
+  }
+};
+
+export const signInOrRegister = async (ctx: Context) => {
+  const { password, confirm_code, email } = ctx.request.body as RequestBody;
+
+  if (!email || (!password && !confirm_code)) {
     ctx.status = 400;
     ctx.body = { message: "Must send some param" };
     return;
@@ -23,86 +109,9 @@ export const signInOrRegister = async (ctx: Context) => {
 
   const cognitoUser = new CognitoUser({ Username: email, Pool: userPool });
 
-  await new Promise<void>((resolve, reject) => {
-    if (confirm_code) {
-      cognitoUser.confirmRegistration(confirm_code, true, (err) => {
-        if (err) {
-          console.log('Error confirming email:', err);
-          ctx.status = 500;
-          ctx.body = { message: 'Error confirming email.', error: err };
-          resolve();
-        } else {
-          ctx.status = 200;
-          ctx.body = { message: 'Email confirmed successfully.' };
-          resolve();
-        }
-      });
-    } else {
-      cognitoUser.authenticateUser(new AuthenticationDetails({ Username: email, Password: password }), {
-        onSuccess: async (result) => {
-          ctx.status = 200;
-          ctx.body = { message: 'User authenticated successfully.', token: result.getAccessToken().getJwtToken() };
-          resolve();
-        },
-        onFailure: async (err) => {
-          if (err.code === 'NotAuthorizedException') {
-            if (confirm_password) {
-              const attributeList = [new CognitoUserAttribute({ Name: 'email', Value: email })];
-
-              userPool.signUp(email, password, attributeList, [], async (signUpErr, signUpResult) => {
-                if (signUpErr) {
-                  ctx.status = 500;
-                  ctx.body = { message: 'Error signing up.', error: signUpErr };
-                  reject();
-                } else if (signUpResult) { // Verifique se signUpResult não é undefined
-                  const cognitoUser = signUpResult.user;
-                  console.log('User registered successfully:', cognitoUser.getUsername());
-              
-                  const newUser = new User();
-                  newUser.cognitoId = cognitoUser.getUsername();
-                  newUser.email = email;
-                  newUser.role = 'user';
-                  newUser.isOnboarded = false;
-                  newUser.createdAt = new Date();
-                  newUser.updatedAt = new Date();
-                  
-              
-                  try {
-                    //debugger;
-                    const userRepository = AppDataSource.getRepository(User);
-                    await userRepository.save(newUser);
-                    ctx.status = 201;
-                    ctx.body = { message: 'User registered successfully, please confirm the email.' };
-                    resolve();
-                  } catch (dbError) {
-                    console.log('Error saving user to database:', dbError);
-                    ctx.status = 500;
-                    ctx.body = { message: 'Error saving user to database.', error: dbError };
-                    reject();
-                  }
-                } else {
-                  ctx.status = 500;
-                  ctx.body = { message: 'Sign-up failed: No result returned.' };
-                  reject();
-                }
-              });
-              
-            } else {
-              ctx.status = 500;
-              ctx.body = { message: "User or password incorrect" };
-              resolve();
-            }
-          } else if (err.code === "UserNotConfirmedException") {
-            ctx.status = 400;
-            ctx.body = { message: 'Please confirm the email' };
-            resolve();
-          } else {
-            ctx.status = 500;
-            ctx.body = { message: 'Authentication failed.', error: err };
-            resolve();
-          }
-        }
-      });
-    }
-  });
+  if (confirm_code) {
+    await confirmUser(cognitoUser, confirm_code, ctx);
+  } else {
+    await loginUser(cognitoUser, password, ctx);
+  }
 };
