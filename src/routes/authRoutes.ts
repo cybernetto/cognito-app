873e4bd0fc0f21fd7b814b9226e940ad8c7aaa08
Raimundo Pessoa <net.almada@gmail.com>
2024-09-22 16:39:29 -0300

    added swagger documentation

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -3,7 +3,40 @@ import { signInOrRegister } from '../controllers/authController';
 
 const router = new Router();
 
-// Rota pública para sign-in/register
+/**
+ * @swagger
+ * /auth:
+ *   post:
+ *     summary: Signin or Signup
+ *     description: Any person can create, login and confirm email using this endpoint.
+ *     security:
+ *       - BearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 description: Email of the user
+ *               password:
+ *                 type: string
+ *                 description: Password of the user
+ *               confirm_password:
+ *                 type: string
+ *                 description: Retype password of the user
+ *               confirm_code:
+ *                 type: string
+ *                 description: Confirmation code sended to email after user creation
+ *               
+ *     responses:
+ *       200:
+ *         description: User created sucessfull
+ *       500:
+ *         description: Error creating user
+ */
 router.post('/auth', signInOrRegister);
 
 export default router;
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,14 +1,98 @@
 import Router from 'koa-router';
-import { getMe, getAllUsers } from '../controllers/userController';
+import { getMe, getAllUsers,updateUser, updateAnyUser } from '../controllers/userController';
 import { authMiddleware } from '../middleware/authMiddleware'; // Middleware de autenticação
 
 const router = new Router();
 
-// Rota protegida por JWT (será implementado no middleware)
+/**
+ * @swagger
+ * /me:
+ *   get:
+ *     summary: Get user information
+ *     description: Returns the authenticated user's information.
+ *     security:
+ *       - BearerAuth: []
+ *     responses:
+ *       200:
+ *         description: User info returned successfully
+ *       401:
+ *         description: Unauthorized
+ */
 router.get('/me', authMiddleware(), getMe);
 
-// Rota para admins, também protegida
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     summary: Get all users
+ *     description: Returns a list of all users (admin only).
+ *     security:
+ *       - BearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Successfully returned user list
+ *       403:
+ *         description: Forbidden
+ */
 router.get('/users', authMiddleware('admin'), getAllUsers);
+
+/**
+ * @swagger
+ * /users:
+ *   put:
+ *     summary: Update any user
+ *     description: Admin can update user information.
+ *     security:
+ *       - BearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               id:
+ *                 type: string
+ *                 description: Cognito ID of the user
+ *               name:
+ *                 type: string
+ *                 description: New name of the user
+ *               role:
+ *                 type: string
+ *                 description: New role of the user
+ *     responses:
+ *       200:
+ *         description: User updated successfully
+ *       500:
+ *         description: Error updating user
+ */
+router.put('/users', authMiddleware('admin'), updateAnyUser);
+
+/**
+ * @swagger
+ * /users:
+ *   post:
+ *     summary: Update self user
+ *     description: Normal user can update self information (name).
+ *     security:
+ *       - BearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: New name of the user
+ *     responses:
+ *       200:
+ *         description: User updated successfully
+ *       500:
+ *         description: Error updating user
+ */
+router.post('/update', authMiddleware(), updateUser )
 
 export default router;
 
